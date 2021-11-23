b7b6bf18d5efe3d517636144fcd5ce881eebe15a
MorganeBENUREAU <morgane.benureau@gmail.com>
2021-11-23 09:20:36 +0100

    init

---
+++ index.ts
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const logger = require('koa-logger');
+
+const Router = require('./app/routers');
+
+const app = new Koa();
+
+const port = process.env.PORT || `3000`;
+
+app.use(bodyParser());
+
+app.use(logger());
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
